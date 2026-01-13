081415f71d589d516cf9e0a5a5dad7963af28d7d
giullianomorroni <giullianomorroni@gmail.com>
2026-01-13 16:25:00 -0300

    deploy

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "${var.function_name}-iam-role"
+  name = "${var.function_name}-iam-role-v1"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
