98e56b6be8a2fa90ec784a42993d6e012e309a65
giullianomorroni <giullianomorroni@gmail.com>
2026-01-19 14:08:27 -0300

    destroy

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -4,9 +4,4 @@ resource "aws_lambda_function" "function" {
   handler          = var.handler
   runtime          = var.runtime
   role             = aws_iam_role.lambda_role.arn
-  source_code_hash = filebase64sha256("deployment/package.zip")
-
-  lifecycle {
-    ignore_changes = [filename]
-  }
 }
